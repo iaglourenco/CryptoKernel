754fc7147ed3e6d28f89870ed548e308f0b151ff
Adriano Munin <adrianomunin@gmail.com>
2019-11-16 16:46:21 -0300

    Testar func read (Obs. trocar diretorio)

--- TesteRead.c
+++ TesteRead.c
@@ -9,14 +9,13 @@ int main (int argc, char *argv[])
     int fd1, sz;
     char *c = (char *) calloc(127, sizeof(char)); 
 
-    fd1 = open("abc.txt", O_RDONLY);
+    fd1 = open("/home/cobaia/Documentos/teste.txt", O_RDONLY);
     if (fd1 == -1) {
         return EXIT_FAILURE;
     }
 
-    sz = read(fd1, c, 127); 
-    c[sz] = '\0'; 
-    printf("%s",c);
+    syscall(549, fd1, c, 32);  
+    printf("%s \n",c);
  
     close(fd1);
     return 0;
